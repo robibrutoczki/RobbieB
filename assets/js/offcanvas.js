0c64f29fbade12fe86dc5d161af8ac41c5696ec9
robibrutoczki <52542134+robibrutoczki@users.noreply.github.com>
2022-09-22 17:59:10 +0100

    Add files via upload

---
+++ assets/js/offcanvas.js
@@ -0,0 +1,16 @@
+(function (open) {
+  'use strict'
+
+  document.querySelector('#navbarSideCollapse').addEventListener('click', function () {
+    document.querySelector('.offcanvas-collapse').classList.toggle('open');
+ 
+  })
+})();
+(function (close) {
+  'use strict'
+
+  document.querySelector('.navbar-collapse').addEventListener('click', function () {
+    document.querySelector('.offcanvas-collapse').classList.toggle('open');
+ 
+  })
+})();
